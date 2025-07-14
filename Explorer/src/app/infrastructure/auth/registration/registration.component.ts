ac1fe64f94ad883f22d541618472aed34d4f3bb8
grujicdd <dusangrujic17@gmail.com>
2025-07-14 11:30:34 +0200

    Feat: Registration
    -Added interests in the form of checkboxes when registering an account

--- Explorer/src/app/infrastructure/auth/registration/registration.component.ts
+++ Explorer/src/app/infrastructure/auth/registration/registration.component.ts
@@ -1,20 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Registration } from '../model/registration.model';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/env/environment';
+
+interface Interest {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'xp-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  interests: Interest[] = [];
 
   registrationForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
@@ -22,7 +26,19 @@ export class RegistrationComponent {
     email: new FormControl('', [Validators.required]),
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
+    interestsIds: new FormControl<number[]>([]) // renamed from interests to interestsIds
   });
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.http.get<Interest[]>(environment.apiHost + 'users/interests')
+      .subscribe(interests => this.interests = interests);
+  }
 
   register(): void {
     const registration: Registration = {
@@ -31,6 +47,7 @@ export class RegistrationComponent {
       email: this.registrationForm.value.email || "",
       username: this.registrationForm.value.username || "",
       password: this.registrationForm.value.password || "",
+      interestsIds: this.registrationForm.value.interestsIds || []
     };
 
     if (this.registrationForm.valid) {
@@ -40,5 +57,19 @@ export class RegistrationComponent {
         },
       });
     }
+  }
+
+  onCheckboxChange(event: any) {
+    const selectedInterests = this.registrationForm.get('interestsIds')?.value || [];
+    const id = parseInt(event.source.value, 10);
+
+    if (event.checked) {
+      selectedInterests.push(id);
+    } else {
+      const index = selectedInterests.indexOf(id);
+      if (index >= 0) selectedInterests.splice(index, 1);
+    }
+
+    this.registrationForm.get('interestsIds')?.setValue(selectedInterests);
   }
 }
